c326cecc0df0b7baea4016983f7ceb9c5bcd2468
Viking0001 <147491906+Viking0001@users.noreply.github.com>
2024-11-08 10:53:02 +0100

    fix grammar

--- 02-while-for/Program.cs
+++ 02-while-for/Program.cs
@@ -7,9 +7,9 @@ namespace mnamina
         static void Main(string[] args)
         {
             /*
-                cykli jsou taky důležitě. Máme dva druhy:
-                    FOR - když víme kolikrát se to bude opakovat
-                    WHILE - když chceme aby se to opakovalo dokud nebude splněné nějaká podmínka
+                Cykly jsou také důležité. Máme dva základní druhy:
+                    FOR - když víme, kolikrát se to bude opakovat
+                    WHILE - když chceme, aby se to opakovalo, dokud nebude splněna nějaká podmínka
             */
 
 
@@ -24,14 +24,14 @@ namespace mnamina
 
             /*
                 WHILE krok po kroku:
-                    1. vytvořím proměnou cislo do ktere uložim nulu
-                    2. zkontroluje zda je podminka, pokud je
-                        - true = udělá to  co je ve složených závorkách (krok 3 a 4)
-                        - false = skočí to za složené závorky a bude pokračovat dát (krok 5)
+                    1. Vytvořím proměnnou cislo a uložíme do ní hodnotu 0
+                    2. Zkontroluje, zda je podmínka splněna. Pokud je:
+                        - true = provede to, co je ve složených závorkách (kroky 3 a 4)
+                        - false = přeskočí to za složené závorky a pokračuje dál (krok 5)
 
-                    podmínka je v našem případě cislo <= 5 a každým projetím ciklu přičtu jedna
-                    takže se ciklus projede 6 (indexujem od nuli)
-                    program vypíše:
+                    Podmínka je v našem případě `cislo <= 5` a každým projetím cyklu přičtu 1.
+                    Tento cyklus se projede 6krát (indexujeme od nuly), tedy pro hodnoty `cislo` od 0 do 5.
+                    Program vypíše:
                         opakuju 0.
                         opakuju 1.
                         opakuju 2.
@@ -44,17 +44,17 @@ namespace mnamina
 
             //       1        2     3
             for (int i = 0; i <= 5; i++) {
-                Console.WriteLine("opakuju " + cislo + ".");
+                Console.WriteLine("opakuju " + i + "."); // 3. Opraveno: měl by se použít i, ne cislo
             }
             Console.WriteLine("konec");
 
             /*
-                FOR má tří části:
-                    1. tahle čast kodu se spustí hned na začátku (v našem vytvoříme proměnou i a uložíme do ní 0)
-                    2. Tohle je dost podobný jak v WHILE. Skontroluju podmínu a pokud je true udělám to co je v složených závorkách (v našem se to bude opakov dokud i nebude 6 nebo vyšší)
-                    3. Tahle část kodu se udělá vždy když program doxjede na konec složených závorkách (v našem případě přičte jedna)
-            
-                výpis bude stejný jako u WHILE:
+                FOR má tři části:
+                    1. Tato část kódu se spustí hned na začátku (vytvoříme proměnnou `i` a uložíme do ní hodnotu 0)
+                    2. Tato část je podobná podmínce ve WHILE. Zkontroluje, zda je podmínka splněna, a pokud ano, provede kód ve složených závorkách (v našem případě se to bude opakovat, dokud `i` nebude větší než 5)
+                    3. Tato část kódu se provede vždy, když program projde jedním cyklem (v našem případě se vždy přičte 1 k proměnné `i`)
+
+                Výstup bude stejný jako u WHILE:
                         opakuju 0.
                         opakuju 1.
                         opakuju 2.
@@ -62,8 +62,7 @@ namespace mnamina
                         opakuju 4.
                         opakuju 5.
                         konec
-             
-             */
+            */
         }
     }
 }
